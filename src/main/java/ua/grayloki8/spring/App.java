fda943ff7b13c177c144723ab7eadf20b680fecb
Gray Loki <skuc090@gmail.com>
2022-07-02 21:02:34 +0300

    create entity Person

---
+++ src/main/java/ua/grayloki8/spring/App.java
@@ -0,0 +1,14 @@
+package ua.grayloki8.spring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+    }
+}
