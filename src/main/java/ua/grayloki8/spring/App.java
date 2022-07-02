b87042bb6abb8387b2b18b24dfbbe661d443f8ad
Gray Loki <skuc090@gmail.com>
2022-07-02 21:12:30 +0300

    run first try

--- src/main/java/ua/grayloki8/spring/App.java
+++ src/main/java/ua/grayloki8/spring/App.java
@@ -1,14 +1,28 @@
 package ua.grayloki8.spring;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import ua.grayloki8.spring.model.Person;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session currentSession = sessionFactory.getCurrentSession();
 
-        System.out.println( "Hello World!" );
+        try {
+            currentSession.beginTransaction();
+            Person person = currentSession.get(Person.class, 1);
+            System.out.println(person);
+            currentSession.getTransaction().commit();
+        } finally {
+
+            sessionFactory.close();
+        }
+        System.out.println("Hello World!");
     }
 }
