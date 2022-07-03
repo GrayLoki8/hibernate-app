7d96a61462e46ab99943c3f58ba5e83fe56227fc
Gray Loki <skuc090@gmail.com>
2022-07-03 20:12:15 +0300

    create entities movie and Actor

---
+++ src/main/java/ua/grayloki8/spring/model/Actor.java
@@ -0,0 +1,73 @@
+package ua.grayloki8.spring.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "actor")
+public class Actor {
+    @Id
+    @Column(name = "actor_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "age")
+    private int age;
+    @ManyToMany
+    @JoinTable(
+            name = "actor_movie",
+            joinColumns = @JoinColumn(name = "actor_id"),
+            inverseJoinColumns = @JoinColumn(name = "movie_id")
+    )
+    private List<Movie> movies;
+    public Actor() {
+    }
+
+    public Actor(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public List<Movie> getMovies() {
+        return movies;
+    }
+
+    public void setMovies(List<Movie> movies) {
+        this.movies = movies;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Actor{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", movies=" + movies +
+                '}';
+    }
+}
